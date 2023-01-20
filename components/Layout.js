827f5ea30200b310f2cad4ddb034e77aa525fde8
Manas Gautam <gautammanas2000@gmail.com>
2023-01-20 16:14:44 +0530

    config, showcase

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
-import styles from '../styles/Layout.module.css'
+import styles from '@/styles/Layout.module.css'
 import Footer from "./Footer";
 import Header from "./Header";
+import Showcase from "./Showcase";
 
-const Layout = ({title, keywords, description, children }) => {
+import { useRouter } from "next/router";
+
+const Layout = ({ title, keywords, description, children }) => {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -13,6 +17,7 @@ const Layout = ({title, keywords, description, children }) => {
       </Head>
 
       <Header />
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>
         {children}
       </div>
--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,18 @@
+import Layout from '../components/Layout';
+import styles from '../styles/404.module.css';
+import Link from 'next/link';
 import { FaExclamationTriangle } from "react-icons/fa";
 
-
-const PageNotFound = () => {
+const NotFoundPage = () => {
   return (
-    <div>
-        <h1>Error Page Not Found</h1>  
-    </div>
+    <Layout title='Page Not Found'>
+      <div className={styles.error}>
+        <h1><FaExclamationTriangle /> 404</h1>
+        <h4>Sorry, there is nothing here.</h4>
+        <Link href='/'>Go Back Home</Link>
+      </div>
+    </Layout>
   )
 }
 
-export default PageNotFound
+export default NotFoundPage
